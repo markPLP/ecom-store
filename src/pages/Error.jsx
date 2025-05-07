ef26bf86e0513831f7270e755450316e47de0c2d
markPLP <mpclaspinas@gmail.com>
2025-05-07 18:07:05 +0800

    fix error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,22 +1,22 @@
-import { useRouteError, Link } from 'react-router-dom';
+import { useRouteError, Link, isRouteErrorResponse } from 'react-router-dom';
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
 
-  if (error.status === 404) {
+  if (isRouteErrorResponse(error) && error.status === 404) {
     return (
-      <main className='grid min-h-[100vh] place-items-center px-8'>
-        <div className='text-center'>
-          <p className='text-9xl font-semibold text-primary'>404</p>
-          <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+      <main className="grid min-h-[100vh] place-items-center px-8">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
             {error.data}
           </h1>
-          <p className='mt-6 text-lg leading-7'>
+          <p className="mt-6 text-lg leading-7">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
-          <div className='mt-10'>
-            <Link className='btn btn-secondary' to='/'>
+          <div className="mt-10">
+            <Link className="btn btn-secondary" to="/">
               Back to homepage
             </Link>
           </div>
@@ -25,8 +25,8 @@ const Error = () => {
     );
   }
   return (
-    <main className='grid min-h-[100vh] place-items-center px-8'>
-      <p className='text-center font-bold text-4xl'>there was an error</p>
+    <main className="grid min-h-[100vh] place-items-center px-8">
+      <p className="text-center font-bold text-4xl">there was an error</p>
     </main>
   );
 };
